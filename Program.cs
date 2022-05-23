e91ddeb318a43d385f91f7f40ebbf3314c42e08e
Sergei <sere0708@mail.ru>
2022-05-23 18:22:40 +0300

    HOME9

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//. Задача. 66
+
+/*
+
+int n, m, s=0;
+Console.Write("Введи значение от: ");
+n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи значение до: ");
+m = Convert.ToInt32(Console.ReadLine());
+for (int i = n; i <= m; i++)
+    s += i;
+Console.WriteLine(s);
+*/
+
+
+//. Задача. 67
+
+/*
+
+int a = int.Parse(Console.ReadLine());
+int s = 0;
+while (a > 0)
+{
+    s = s + a % 10;
+    a = a /10 ;
+ 
+}
+Console.WriteLine(s);
+
+*/
+
